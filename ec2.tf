a2c17652caf0c42da33fbc630fe098fd7a44ba5e
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2025-11-19 15:44:49 +0000

    tf ec2 with sec group done

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "example" {
   ami           = "${var.my-os-image}"
   instance_type = var.machine_size
   key_name = var.private_key_name
+  # adding security group to ec2 vm 
+  vpc_security_group_ids = [aws_security_group.ashu_sec-groups.id]
 
   tags = {
     Name = var.vm_name
--- security-groups.tf
+++ security-groups.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "ashu_sec-groups" {
   vpc_id      = data.aws_vpc.ashu-exiting-vpc.id
 
   tags = {
-    Name = "ashu_firewall_rules"
+    Name = "ashu_firewall_rules_final"
   }
 }
 # allow all outgoing ports for ipv4 
@@ -13,3 +13,22 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+# allow 22 
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.ashu_sec-groups.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+## allow  80 port from everywhere 
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_http" {
+  security_group_id = aws_security_group.ashu_sec-groups.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
